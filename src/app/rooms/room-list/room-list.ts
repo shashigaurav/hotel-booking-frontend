9ae172262c07f38ec43f7dc999bc249938db2c42
Shashi Guarav <shashigaurav789@gmail.com>
2025-12-31 01:02:16 +0530

    add serach bar

--- src/app/rooms/room-list/room-list.ts
+++ src/app/rooms/room-list/room-list.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { RoomService } from '../../core/services/room';
-
 
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './room-list.html',
   styleUrls: ['./room-list.css']
 })
 export class RoomList implements OnInit {
 
   rooms: any[] = [];
+  filteredRooms: any[] = [];
+  searchText: string = '';
   loading = true;
 
   constructor(private roomService: RoomService) {}
@@ -22,6 +24,7 @@ export class RoomList implements OnInit {
     this.roomService.getAllRooms().subscribe({
       next: (data) => {
         this.rooms = data;
+        this.filteredRooms = data; // 👈 important
         this.loading = false;
       },
       error: (err) => {
@@ -29,5 +32,15 @@ export class RoomList implements OnInit {
         this.loading = false;
       }
     });
+  }
+
+  filterRooms() {
+    const text = this.searchText.toLowerCase();
+
+    this.filteredRooms = this.rooms.filter(room =>
+      room.roomType?.toLowerCase().includes(text) ||
+      room.roomNumber?.toString().includes(text) ||
+      room.location?.toLowerCase().includes(text)   // agar backend me ho
+    );
   }
 }
